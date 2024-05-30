c6339e6e0c2a8f0c0e303452fd5f7cac00aec386
Isabel Lopez Murillo <isaana@email.unc.edu>
2024-05-29 23:14:52 -0400

    Style footer + add clock

---
+++ static/index.js
@@ -0,0 +1,37 @@
+
+// footer functions
+
+let windowsButton = document.getElementsByClassName('windows-button')[0];
+
+windowsButton.addEventListener("click", function() {
+});
+
+// date
+function clock() {
+    function updateClock() {
+        const now = new Date();
+        const utc = now.getTime() + (now.getTimezoneOffset() * 60000);
+
+        const isDST = (now.getMonth() > 2 && now.getMonth() < 11);
+        const estOffset = isDST ? -4 : -5; 
+        const estTime = new Date(utc + (3600000 * estOffset));
+
+        let hours = estTime.getHours();
+        const ampm = hours >= 12 ? 'PM' : 'AM';
+        hours = hours % 12;
+        hours = hours ? hours : 12; // the hour '0' should be '12'
+        const minutes = estTime.getMinutes();
+        const seconds = estTime.getSeconds();
+        const formattedTime = 
+            (hours < 10 ? "0" + hours : hours) + " : " + 
+            (minutes < 10 ? "0" + minutes : minutes) + " : " + 
+            (seconds < 10 ? "0" + seconds : seconds) + " " + ampm;
+
+        console.log(formattedTime);
+        document.getElementById('clock').innerText = formattedTime;
+        // document.getElementById('date').innerHTML = days[0] + " " + monthNames[now.getMonth()] + " " + now.getDate();
+    }
+    setInterval(updateClock, 1000);
+}
+
+clock();
